e827b5704864bfb7b85acd85cbca020eecd33608
daryl <darylpoon@gmail.com>
2020-02-27 23:38:26 +0800

    updated chat user count

--- server.js
+++ server.js
@@ -84,27 +84,9 @@ io.on('connection', function (socket) {
     });
     socket.on("chat message", (msg) => {
         console.log(`${id}: ${msg}`);
-        io.emit("chat message", { id, msg });
+        io.emit("chat message", { id, msg }, userCount);
     });
 });
-
-// io.on('connection', function (client) {
-//     console.log('a user connected');
-//     client.on('register', handleRegister)
-//     client.on('join', handleJoin)
-//     client.on('leave', handleLeave)
-//     client.on('message', handleMessage)
-//     client.on('chatrooms', handleGetChatrooms)
-//     client.on('availableUsers', handleGetAvailableUsers)
-//     client.on('disconnect', function () {
-//         console.log('client disconnect...', client.id)
-//         handleDisconnect()
-//     })
-//     client.on('error', function (err) {
-//         console.log('received error from client:', client.id)
-//         console.log(err)
-//     })
-// })
 
 
 http.listen(PORT, () => {
